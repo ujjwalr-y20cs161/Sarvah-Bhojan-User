3ed59dd961d2bbf947d9b07a18bd1f7170e5d454
Ujjwal R <ujjwalrsanagapati@gmail.com>
2024-06-22 10:20:05 +0530

    Added : Register View Model,HelpText

--- app/src/main/java/com/example/fooddeliveryuser/Register.java
+++ app/src/main/java/com/example/fooddeliveryuser/Register.java
@@ -9,10 +9,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.fooddeliveryuser.databinding.ActivityRegisterBinding;
 import com.example.fooddeliveryuser.viewmodels.LoginViewModel;
+import com.example.fooddeliveryuser.viewmodels.RegisterViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -20,7 +22,7 @@ public class Register extends AppCompatActivity {
 
     ActivityRegisterBinding binding;
 
-    LoginViewModel loginViewModel;
+    RegisterViewModel registerViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,18 +30,31 @@ public class Register extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+        registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
+
+        registerViewModel.getPassedRegister().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(aBoolean){
+                    startActivity(new Intent(getApplicationContext(),AddressPicker.class));
+                    finish();
+                }else if(!registerViewModel.getRegisterCheck().equals("")){
+                    notifyHelpText(registerViewModel.getRegisterCheck());
+                }
+            }
+        });
+
 
 
         // Set onClickListener for register button
-        binding.buttonCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerUser();
-                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-            }
+        binding.buttonCreate.setOnClickListener(v->{
+                registerViewModel.RegisterUser(binding.editTextUsername.getText().toString(),binding.editTextEmail.getText().toString(),binding.editTextPhone.getText().toString(),binding.editTextPassword.getText().toString(),binding.termAndCondCheck.isChecked());
         });
+
     }
 
-    private void registerUser() {
+    private void notifyHelpText(String Message){
+        binding.helpText.setText(Message);
+        binding.helpText.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/com/example/fooddeliveryuser/viewmodels/RegisterViewModel.java
@@ -0,0 +1,81 @@
+package com.example.fooddeliveryuser.viewmodels;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+
+    private String registerCheck;
+    MutableLiveData<Boolean> passedRegister;
+
+    public RegisterViewModel() {
+        this.passedRegister = new MutableLiveData<>(false);
+        registerCheck = "";
+    }
+
+
+
+    private void setRegisterCheck(String registerCheck) {
+        this.registerCheck = registerCheck;
+    }
+
+    public MutableLiveData<Boolean> getPassedRegister() {
+        return passedRegister;
+    }
+
+    private void setPassedRegister(Boolean passedRegister) {
+        this.passedRegister.setValue(passedRegister);
+    }
+
+
+
+    public String getRegisterCheck() {
+        return registerCheck;
+    }
+
+    public void RegisterUser(String UserName,String EmailAddress,String PhoneNumber,String Password, Boolean checked){
+//        Logic to check whether UserName or Email Address or Phone Number is repeated:
+//        If yes : sent a warning string
+        if(UserName.equals("User101") || EmailAddress.equals("user101@gmail.com")|| PhoneNumber.equals("9876543210")){
+            setRegisterCheck("User Already Exists");
+            setPassedRegister(false);
+            Log.e("Register Log","User Already Exists");
+        }        //        if UserName doesn't meet requirement
+        else if (UserName.length() < 4) {
+            setRegisterCheck("UserName Should be More than 4 Characters");
+            setPassedRegister(false);
+            Log.e("Register Log","UserName Should be More than 4 Characters");
+        }
+//        if Phone Number is not valid
+        else if(PhoneNumber.length() < 10){
+            setRegisterCheck("Invalid Phone Number");
+            setPassedRegister(false);
+            Log.e("Register Log","Invalid Phone Number");
+        }
+        //        if Email Address is not valid
+        else if(!EmailAddress.contains("@") || !EmailAddress.contains(".")){
+//             replace with a proper email checker
+            setRegisterCheck("Invalid Email Address");
+            setPassedRegister(false);
+            Log.e("Register Log","Invalid Email Address");
+        }
+
+        //        if password does not meet requirements send warning
+        else if(Password.length() < 8){
+            if(Password.equals("")){setRegisterCheck("Please enter the Password");
+                setPassedRegister(false);
+                Log.e("Register Log", "Please enter the Password");}
+            else {
+                setRegisterCheck("Password Should be More than 8 Characters");
+                setPassedRegister(false);
+                Log.e("Register Log", "Password Should be More than 8 Characters");
+            }
+        }
+        else{
+            setPassedRegister(true);
+        }
+    }
+
+}
